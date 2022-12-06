a5abe11802882688b0bcf3fc8d9412dbaaf94076
curry <curry.dulcie@gmail.com>
2022-12-06 00:16:36 -0500

    remove log

--- 06/06-02.ts
+++ 06/06-02.ts
@@ -1,7 +1,6 @@
 let fs = require('fs');
 let text: any[] = fs.readFileSync('./06-01-input.txt').toString().split('');
 let found = false;
-console.log(text.length)
 for(let i = 0; i < text.length-13 && !found; i++){
     const currentToSearch = text.slice(i, i+14);
     currentToSearch.sort((a,b) => a.localeCompare(b))
